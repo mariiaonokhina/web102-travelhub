c09097e90fb988a08a8be83dfacbdc0f60c79e7b
mariiaonokhina <83292730+mariiaonokhina@users.noreply.github.com>
2023-04-15 17:58:16 -0400

    Navbar finished

---
+++ travelhub/src/routes/Layout.jsx
@@ -0,0 +1,39 @@
+import { Outlet, Link } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul className="navbar-container">
+          <li className="logo-link navbar-item" key="logo-button">
+            <Link className='logo link-item' to="/web102-travelhub/">
+              <img id='logo-img' src='logo.png'></img>
+            </Link>
+          </li>
+
+        <div className='right-navbar'>
+          <li className="search navbar-item" key="search">
+            <SearchBar />
+          </li>
+
+          <li className="home-link navbar-item" key="home-button">
+            <Link className='link-item' to="/web102-travelhub/">
+              Home
+            </Link>
+          </li>
+
+          <li className="create-link navbar-item" key="create-button">
+            <Link className='link-item' to="/web102-travelhub/create">
+              Create New Post
+            </Link>
+          </li>
+          </div>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
